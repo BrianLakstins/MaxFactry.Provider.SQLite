600273c546b4d457ec1948c4a16f9b9f7061d55b
BrianLakstins <2887390+BrianLakstins@users.noreply.github.com>
2025-06-23 09:08:06 -0400

    Update NuGet package

--- MaxFactry.Provider.SQLiteProvider-NF-2.0/Properties/AssemblyInfo.cs
+++ MaxFactry.Provider.SQLiteProvider-NF-2.0/Properties/AssemblyInfo.cs
@@ -66,6 +66,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.24.3")]
-[assembly: AssemblyFileVersion("2024.03.31.01")]
-[assembly: AssemblyInformationalVersion("1.0.24.3")]
+[assembly: AssemblyVersion("1.0.24.56")]
+[assembly: AssemblyFileVersion("2025.06.23.01")]
+[assembly: AssemblyInformationalVersion("1.0.24.56")]
